dbd215a5649f6b2d19e2e6541c5deeace52b12b2
domi <dominik.felpec@gmail.com>
2024-05-31 10:23:55 +0200

    cross compile and embed image

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+x86_64-w64-mingw32-gcc \
+-mwindows -Wall -Wextra -ggdb \
+-I"./include" \
+-I"./assets" \
+-o bin/main \
+src/main.c \
+-L./lib \
+-l:libraylib.a \
+-lwinmm -lgdi32 \
+-lm
